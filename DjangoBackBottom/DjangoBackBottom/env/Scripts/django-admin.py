d783733753ebb9995ea5adc4e6febb249257ab9e
Felicia <feliciajarosinski@gmail.com>
2020-07-21 09:07:15 -0500

    initial commit

---
+++ DjangoBackBottom/DjangoBackBottom/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\fjarosinski\Courses\profiles-rest-api\DjangoBackBottom\DjangoBackBottom\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
